b0192976b8ef2e5ed89634b01a8141f3347c0551
Abdallah Abedraba <aabedraba@gmail.com>
2021-04-25 21:56:52 +0200

    Add favicon·

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -35,6 +35,7 @@ export const Layout: FC = ({ children }) => {
       <Head>
         <title>Abdallah Abedraba</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/png" href="/favicon.png"/>
       </Head>
       <div
         style={{
