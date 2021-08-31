1128e271b40a231969fcb3234cf8cdd3bfc08b17
Abdallah Abedraba <aabedraba@gmail.com>
2021-08-31 18:00:06 +0200

    Add plausible analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,16 @@ import "../styles/globals.css";
 import { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <script
+        defer
+        data-domain="aabedraba.com"
+        src="https://plausible.io/js/plausible.js"
+      ></script>
+    </>
+  );
 }
 
 export default MyApp;
