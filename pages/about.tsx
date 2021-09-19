c7c94396a8ecfd69344cbc5ed494cbc53a1a559a
Abdallah Abedraba <aabedraba@gmail.com>
2021-09-19 19:28:40 +0200

    Update aboutme

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,9 +4,9 @@ const About = () => {
   return (
     <Layout>
       <p className="text-justify">
-        My name is Abdallah and I'm a Software Engineer at{" "}
+        I'm Abdallah and I currently work as a Software Engineer at{" "}
         <a href="https://www.greenmobility.com/dk/en/">GreenMobility</a>{" "}
-        building cool electric cars sharing stuff. Deeply passionate about
+        building cool electric cars sharing platform. Deeply passionate about
         innovation and communities, participating as a member of Google
         Developer Groups and previously working with the DevRel team at Google.
         Love statically typed programming languages, anything jazz, FOSS, hiking
