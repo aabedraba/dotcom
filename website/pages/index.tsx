444e5e52f01892502e60d4a3a1be63f59493b2c1
Abdallah Abedraba <aabedraba@gmail.com>
2022-01-09 16:27:39 +0400

    style

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -17,8 +17,7 @@ type PageProps = {
 
 const LoadingLastPlayedSong = () => {
   return (
-    <div className="animate-pulse space-y-4">
-      <div className="w-72 h-3 bg-gray-200 rounded"></div>
+    <div className="animate-pulse space-y-4 mt-1">
       <div className="w-56 h-3 bg-gray-200 rounded"></div>
       <div className="w-56 h-3 bg-gray-200 rounded"></div>
       <div className="w-40 h-3 bg-gray-200 rounded"></div>
@@ -63,12 +62,9 @@ const Index = ({ postList }: PageProps) => {
     <Layout>
       <div className="space-y-7">
         <div>
+          <span className="text-gray-900">Recently played on my Spotify</span>
           {songDetails ? (
             <div className="flex flex-col">
-              <span className="text-gray-900">
-                {" "}
-                Recently played on my Spotify
-              </span>
               <span>
                 <a href={songDetails.songUrl}>
                   {songDetails.title}
