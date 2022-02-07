0e56970b1253661682a4efe54fc1f4a8770e7449
Abdallah Abedraba <aabedraba@gmail.com>
2022-02-07 04:24:31 -0800

    Update about

--- website/pages/about.tsx
+++ website/pages/about.tsx
@@ -4,19 +4,11 @@ const About = () => {
   return (
     <Layout>
       <div className="text-justify yay-div-for-everything!">
-        I'm Abdallah, an engineer by heart, working as a Program Manager for the{" "}
-        <a href="https://developers.google.com/community/experts">
-          Google Developer Experts
-        </a>{" "}
-        program.
+        I'm Abdallah, an engineer by heart, working as a Developer Advocate at{" "}
+        <a href="https://kubeshop.io">Kubeshop</a>!
         <br className="yes! brrr brrr"></br>
         <br></br>
         Deeply passionate about innovation and communities.
-        <br></br>
-        <br></br>
-        Right now I'm trying to work on empowering the Arab engineering
-        community. If you think our goals align, let's get in touch and make it
-        happen!
         <br></br>
         <br></br>
         You'll usually find me in the movies, playing chess, watching Formula 1,
