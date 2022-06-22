110d18c957fcc54c65ea4ba08875bd55c65189ef
Abdallah Abedraba <aabedraba@gmail.com>
2022-06-22 16:07:22 +0400

    Fix link

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -35,7 +35,7 @@ const Index = ({ postList }: PageProps) => {
 
   useEffect(() => {
     async function getCurrentSongDetails() {
-      const request = await fetch("aabedraba-spotify-auth.deno.dev");
+      const request = await fetch("https://aabedraba-spotify-auth.deno.dev");
 
       if (request.status === 401) {
         return;
