eeb1bb3ed477eb2e19890ad836c5ccf757030dd6
Abdallah Abedraba <aabedraba@gmail.com>
2022-06-23 01:58:34 +0400

    Remove token

--- bot/deno/deno-index.ts
+++ bot/deno/deno-index.ts
@@ -1,9 +1,21 @@
 import { Bot } from "https://deno.land/x/grammy/mod.ts";
+import { createClient } from "https://deno.land/x/supabase/mod.ts";
 
-// Create bot object
-const bot = new Bot("5033857686:AAFVqGnluBgcatHJ4JDbTyluwe7BJQytRuc"); // <-- place your bot token inside this string
+const telegramToken = Deno.env.get("TELEGRAM_BOT_TOKEN");
+const supabaseLink = Deno.env.get("SUPABASE_LINK");
+const supabaseKey = Deno.env.get("SUPABASE_KEY");
+
+if (!telegramToken || !supabaseKey || !supabaseLink) {
+  throw new Error("Environment variables are not defined");
+}
+
+const bot = new Bot(telegramToken); // <-- place your bot token inside this string
+
+const supabase = createClient(supabaseLink, supabaseKey);
 
 bot.hears(/\/new_tweet (.+)/, async (ctx) => {
+  const bucketList = await supabase.storage.listBuckets();
+  console.log(bucketList);
   return ctx.reply(ctx.match[1]);
 });
 
