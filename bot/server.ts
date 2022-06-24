be47b249224c382657647dc14c7b6824ecf6aa07
Abdallah Abedraba <aabedraba@gmail.com>
2022-06-24 11:24:41 +0400

    Add debug info

--- bot/server.ts
+++ bot/server.ts
@@ -5,9 +5,17 @@ import { handleRequest } from "./bot.ts";
 const app = new Application(); // or whatever you're using
 const router = new Router();
 
+app.use(async (ctx, next) => {
+  console.log("Request");
+  console.log(ctx.request.method);
+  console.log(ctx.request.url);
+  next();
+});
+
 router.get("/" + config.telegramToken, async (ctx) => {
   return (ctx.response.body = "Hello!");
 });
+
 router.post("/" + config.telegramToken, async (ctx) => {
   return await handleRequest();
 });
