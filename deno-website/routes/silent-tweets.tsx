548c9ccac5d9f220ebce4cc415bca3e95bca117e
Abdallah Abedraba <aabedraba@gmail.com>
2022-06-30 00:54:51 +0400

    migrate silent-tweets

--- deno-website/routes/silent-tweets.tsx
+++ deno-website/routes/silent-tweets.tsx
@@ -3,22 +3,29 @@ import { h } from "preact";
 import { Layout } from "../components/Layout.tsx";
 import { createClient } from "supabase";
 import { tw } from "twind";
-import { PageProps } from "$fresh/server.ts";
+import { PageProps, Handlers } from "$fresh/server.ts";
 
 type Tweet = {
   text: string;
   date: number;
 };
 
-const SilentTweets = (props: PageProps) => {
-  const tweets: Tweet[] = [{ text: "Hello Fresh!", date: Date.now() }];
+export const handler: Handlers<Tweet[] | null> = {
+  async GET(_, ctx) {
+    const tweetList = await getTweets();
+
+    return ctx.render(tweetList);
+  },
+};
+
+const SilentTweets = ({ data, ...props }: PageProps<Tweet[]>) => {
   return (
     <Layout url={props.url}>
       <h1 className={tw`text-2xl`}>
         A collection of micro-thoughts from daily life
       </h1>
       <div>
-        {tweets
+        {data
           .filter((tweet) => tweet != null)
           .sort((a, b) => (a < b ? 1 : -1))
           .map((tweet) => {
@@ -36,32 +43,30 @@ const SilentTweets = (props: PageProps) => {
   );
 };
 
-// export const getStaticProps: GetStaticProps<{ data: Tweet[] }> = async () => {
-//   const supabaseKey = process.env.SUPABASE_KEY;
-//   const supabaseUrl = process.env.SUPABASE_URL;
+const getTweets = async (): Promise<Tweet[]> => {
+  const supabaseKey = Deno.env.get("SUPABASE_KEY");
+  const supabaseUrl = Deno.env.get("SUPABASE_URL");
 
-//   if (!supabaseKey || !supabaseUrl) {
-//     throw new Error("Supabase environment variables not defined");
-//   }
+  if (!supabaseKey || !supabaseUrl) {
+    throw new Error("Supabase environment variables not defined");
+  }
 
-//   const supabase = createClient(supabaseUrl, supabaseKey);
-//   const query = await supabase.from("silent_tweets").select();
-//   const data = query.data;
+  const supabase = createClient(supabaseUrl, supabaseKey);
+  const query = await supabase.from("silent_tweets").select();
+  const data = query.data;
 
-//   if (!data) {
-//     throw new Error("No data retrieved from database");
-//   }
+  if (!data) {
+    throw new Error("No data retrieved from database");
+  }
 
-//   return {
-//     props: {
-//       data: data.map((row) => {
-//         return {
-//           text: row.tweet,
-//           date: row.created_at,
-//         };
-//       }),
-//     },
-//   };
-// };
+  const tweets = data.map((row) => {
+    return {
+      text: row.tweet,
+      date: row.created_at,
+    };
+  });
+
+  return tweets;
+};
 
 export default SilentTweets;
