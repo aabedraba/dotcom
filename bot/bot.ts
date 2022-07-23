451c6814b54c29cdc60de4a2f659e2cf22b63987
Abdallah Abedraba <aabedraba@gmail.com>
2022-07-23 13:30:15 +0200

    remove cloudflare deployment

--- bot/bot.ts
+++ bot/bot.ts
@@ -10,11 +10,6 @@ bot.hears(/\/new_tweet (.+)/, async (ctx) => {
   ctx.reply("Silent Tweet received. Uploading to database...");
   await supabase.from("silent_tweets").insert([{ tweet: ctx.match[1] }]);
 
-  ctx.reply("Triggering new website deployment...");
-  await fetch(config.deployHook, {
-    method: "POST",
-  });
-
   return ctx.reply("Silent Tweet deployed!");
 });
 
