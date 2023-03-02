e2a56990fe9715d3968961acffe359140e8a79d4
Abdallah Abedraba <aabedraba@gmail.com>
2023-03-02 15:20:05 +0400

    add vercel analytics
    Signed-off-by: Abdallah Abedraba <aabedraba@gmail.com>

--- www/pages/_app.tsx
+++ www/pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />;
+      <Analytics />
     </>
   );
 }
