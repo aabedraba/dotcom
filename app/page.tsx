c8da7806b7f1c5f8bb2cbcc3617e06a71c527d01
abdallah <aabedraba@gmail.com>
2024-09-23 20:46:11 +0200

    make it pro and less personal

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,34 @@
 import Link from "next/link";
-import { getBlogposts } from "../lib/blogposts";
-import { CurrentSong, LoadingLastPlayedSong } from "./current-song";
-import { Suspense } from "react";
+import articleList from "./external-articles.json";
 
 const Index = async () => {
-  const postList = getBlogposts();
-
   return (
     <div className="space-y-7">
-      <Suspense fallback={<LoadingLastPlayedSong />}>
-        <CurrentSong />
-      </Suspense>
-
       <ul className="space-y-5">
-        {postList.map((post) => {
-          return (
-            <li key={post.title} className="flex">
-              <span className="block pr-10 text-gray-700">
-                {new Date(post.date).toISOString().split("T")[0]}
-              </span>
-              <div className="flex flex-col">
-                <Link href={`/posts/${post.slug}`}>{post.title}</Link>
+        {articleList
+          .sort((a, b) =>
+            new Date(a.publishDate) < new Date(b.publishDate) ? 1 : -1,
+          )
+          .map((post) => {
+            return (
+              <li key={post.title} className="flex">
+                <span className="block pr-10 text-gray-700">
+                  {new Date(post.publishDate).toISOString().split("T")[0]}
+                </span>
+                <div className="flex flex-col">
+                  <Link href={post.href}>{post.title}</Link>
 
-                <div className="flex">
-                  {post.tags.map((tag) => {
-                    return <p className="text-sm text-gray-500 mr-2">#{tag}</p>;
-                  })}
+                  <div className="flex">
+                    {post.category.map((tag) => {
+                      return (
+                        <p className="text-sm text-gray-500 mr-2">#{tag}</p>
+                      );
+                    })}
+                  </div>
                 </div>
-              </div>
-            </li>
-          );
-        })}
+              </li>
+            );
+          })}
       </ul>
     </div>
   );
